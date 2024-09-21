e98ac84c6938480c1dcae22bada2ab9b280b3bd7
filobatire Henein <94156757+CoptEgypt@users.noreply.github.com>
2024-09-21 12:33:43 -0700

    asx

--- Appointment-Schedular/MinimalApiProject/obj/Debug/net8.0/MinimalApiProject.AssemblyInfo.cs
+++ Appointment-Schedular/MinimalApiProject/obj/Debug/net8.0/MinimalApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4a3e9b1a4509ac024ff4e25df78e1501cdab28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4998a4174299be151e9fae0cbf97f46f75dc2a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
