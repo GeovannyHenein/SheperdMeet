0af4fa8c15c5f209f4fbb9541402e315c3b35de6
GeovannyHenein <77251112+GeovannyHenein@users.noreply.github.com>
2024-09-22 16:58:52 -0700

    commit

--- Appointment-Schedular/MinimalApiProject/obj/Debug/net8.0/MinimalApiProject.AssemblyInfo.cs
+++ Appointment-Schedular/MinimalApiProject/obj/Debug/net8.0/MinimalApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fba3e970c4ed21b0534b2f7d6da9eefcaa3b0fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35eb114e8d05387ce920a9e37d847d083d1b0aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
